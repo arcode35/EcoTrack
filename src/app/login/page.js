07a4e19b7cb72623ed46adf1b98628e7518e4aff
MahdMalik <mahdmahdmalik@gmail.com>
2025-04-02 16:47:00 -0500

    sign up page implemented

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,18 @@
-"use client";
+'use client'
+import {React, useState} from "react";
 import Link from "next/link";
-import { Button, TextField } from "@mui/material";
-import { useState } from "react";
 import axios from "axios";
+import {
+  Box,
+  CssBaseline,
+  TextField,
+  Button,
+  Typography,
+  Paper,
+  AppBar,
+  Toolbar
+} from "@mui/material";
+import { motion } from "framer-motion";
 
 export default function Login() {
   const [username, setUsername] = useState("");
@@ -17,7 +27,8 @@ export default function Login() {
   // This is the function sending the username and passwrod to the backend.
   // 3/16 line 23 is undefined
   const sendLoginInfo = async () => {
-    try {
+    try 
+    {
       const response = await axios.post(
         "http://localhost:5000/users/login_user",
         {
@@ -38,46 +49,174 @@ export default function Login() {
       {
         alert(responseData.message)
       }
-    } catch (error) {
+    } 
+    catch (error) 
+    {
       console.log("error communciating with backend!");
     }
   };
 
   return (
-    <div>
-      {/* Sends user back to the home page. */}
-      <Button variant="contained" component={Link} href="/">
-        BACK
-      </Button>
-      <p>
-        Need to create account? Click here:
-        <Button variant="contained" component={Link} href="/register">
-          REGISTER
-        </Button>
-      </p>
-      <p>Put your Login Info here.</p>
-      <p>
-        USERNAME:
-        {/*This is a field where they can put their username, updates in real time.*/}
-        <TextField
-          placeholder="Username here"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        ></TextField>
-      </p>
-      <p>
-        PASSWORD:
-        {/* This will let user put password, updates in real time. */}
-        <TextField
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></TextField>
-      </p>
-      {/* On click, calls login function. */}
-      <Button variant="contained" onClick={sendLoginInfo}>
-        Sign In
-      </Button>
-    </div>
+    <>
+      <CssBaseline />
+      {/* Toolbar with Back & Sign Up buttons */}
+      <AppBar position="static" sx={{ background: "black" }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Button
+            variant="contained"
+            component={Link}
+            href="/"
+            sx={{
+              textTransform: "none",
+              background: "linear-gradient(90deg, #3DC787 0%, #55C923 100%)",
+              boxShadow: "0 4px 20px rgba(85, 201, 35, 0.3)",
+              "&:hover": {
+                background:
+                  "linear-gradient(90deg, #55C923 0%, #3DC787 100%)",
+              },
+            }}
+          >
+            Back To Home
+          </Button>
+          <Button
+            variant="contained"
+            component={Link}
+            href="/register"
+            sx={{
+              textTransform: "none",
+              background: "linear-gradient(90deg, #3DC787 0%, #55C923 100%)",
+              boxShadow: "0 4px 20px rgba(85, 201, 35, 0.3)",
+              "&:hover": {
+                background:
+                  "linear-gradient(90deg, #55C923 0%, #3DC787 100%)",
+              },
+            }}
+          >
+            Need an Account? Register Here
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        sx={{
+          backgroundColor: "rgb(0, 0, 0)",
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 2,
+        }}
+      >
+        {/* Login Card */}
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <Paper
+            elevation={4}
+            sx={{
+              padding: 5,
+              borderRadius: 4,
+              background: "rgba(255, 255, 255, 0.03)",
+              backdropFilter: "blur(10px)",
+              border: "1px solid rgba(255, 255, 255, 0.1)",
+              color: "#fff",
+              width: 400,
+              textAlign: "center",
+            }}
+          >
+            <Box mb={3}>
+              <img src="EcoTrack.svg" alt="EcoTrack Logo" width="100px" />
+            </Box>
+            <Typography
+              variant="h5"
+              sx={{
+                fontFamily: "Quicksand, sans-serif",
+                fontWeight: 500,
+                mb: 3,
+              }}
+            >
+              Sign In
+            </Typography>
+            <Box component="form" noValidate autoComplete="off" sx={{ mt: -2 }}>
+              <TextField
+                fullWidth
+                placeholder="Username here"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}                
+                variant="outlined"
+                margin="normal"
+                InputLabelProps={{
+                  style: { color: "#aaa", fontFamily: "Quicksand, sans-serif" },
+                }}
+                InputProps={{ style: { fontFamily: "Quicksand, sans-serif" } }}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    color: "#fff",
+                    "& fieldset": {
+                      borderColor: "#808080", // default
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#55C923", // on hover
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#55C923", // on focus
+                    },
+                  },
+                }}
+              />
+
+              <TextField
+                fullWidth
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                variant="outlined"
+                margin="normal"
+                InputLabelProps={{
+                  style: { color: "#aaa", fontFamily: "Quicksand, sans-serif" },
+                }}
+                InputProps={{ style: { fontFamily: "Quicksand, sans-serif" } }}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    color: "#fff",
+                    "& fieldset": {
+                      borderColor: "#808080", // default
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#55C923", // on hover
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#55C923", // on focus
+                    },
+                  },
+                }}
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                onClick={sendLoginInfo}
+                sx={{
+                  mt: 3,
+                  fontFamily: "Quicksand, sans-serif",
+                  textTransform: "none",
+                  fontWeight: 500,
+                  background:
+                    "linear-gradient(90deg, #3DC787 0%, #55C923 100%)",
+                  boxShadow: "0 4px 20px rgba(85, 201, 35, 0.3)",
+                  "&:hover": {
+                    background:
+                      "linear-gradient(90deg, #55C923 0%, #3DC787 100%)",
+                  },
+                }}
+              >
+                Log In
+              </Button>
+            </Box>
+          </Paper>
+        </motion.div>
+      </Box>
+    </>
   );
-}
+};
