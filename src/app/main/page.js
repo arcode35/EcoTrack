5402463898e2bd102f5215d73fda120d9dd2b03e
MahdMalik <mahdmahdmalik@gmail.com>
2025-04-02 17:27:15 -0500

    UI fully merged

--- src/app/main/page.js
+++ src/app/main/page.js
@@ -1,23 +1,47 @@
 'use client'
-import Link from 'next/link';
-import { Button, TextField } from "@mui/material";
-import { useState } from 'react';
+import {React, useState} from "react";
 import axios from "axios";
+import {
+  Box,
+  CssBaseline,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+  ThemeProvider,
+  createTheme,
+  Button,
+  TextField,
+} from "@mui/material";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import InsightsIcon from "@mui/icons-material/Insights";
+import ScheduleIcon from "@mui/icons-material/Schedule";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import BoltIcon from "@mui/icons-material/Bolt";
+import SpaIcon from "@mui/icons-material/Spa";
+import LiveEnergyChart from "../../components/LiveEnergyChart";
+const theme = createTheme({
+  typography: {
+    fontFamily: "Quicksand, sans-serif",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          WebkitFontSmoothing: "antialiased",
+          MozOsxFontSmoothing: "grayscale",
+        },
+      },
+    },
+  },
+});
 
-export default function Main()
-{
-    const [washerHours, setWashHours] = useState()
-    const [dryerHours, setDryerHours] = useState()
-    const [homeArea, setHomeArea] = useState()
-    const [numRooms, setNumRooms] = useState()
-    const [numBedrooms, setNumBedrooms] = useState()
-    const [numPeople, setNumPeople] = useState()
-    const [numHeatCoolFootage, setNumHeatCoolFootage] = useState()
-    const [numHeatCoolDays, setNumheatCoolDays] = useState()
+export default function Main() {
     const [latitude, setLatitude] = useState()
     const [longitude, setLongitude] = useState()
 
-    // This is the function sending the username and passwrod to the backend.
     const testPythonCall = async() => {
         const response = await axios.get("http://localhost:5001/python/message", {
         })
@@ -30,7 +54,7 @@ export default function Main()
         localStorage.setItem("username", "")
         redirectFunction()
     }
-
+  
     const redirectFunction = async() => {
         //checks if we're actually not logged in, and we need to go back to the main menu
         if(localStorage.getItem("username") === null || localStorage.getItem("username") === "")
@@ -61,43 +85,192 @@ export default function Main()
     }
 
     return (
-        <div>   
-            <h1>If you don't know any of these, leave them empty.</h1>
-            <Button variant='contained' onClick={logoutUser}>LOGOUT</Button>
-            <p>Here, put in the # of hours you use your washer monthly:
-                <TextField placeholder="Washer Hours" value={washerHours} onChange={(e) => setWashHours(e.target.value)}></TextField>
-            </p>
-            <p>Here, put in the # of hours you use your dryer monthly:
-                <TextField placeholder="Dryer Hours" value={dryerHours} onChange={(e) => setDryerHours(e.target.value)}></TextField>
-            </p>
-            <p>Here, square footage of your home:
-                <TextField placeholder="Home Area" value={homeArea} onChange={(e) => setHomeArea(e.target.value)}></TextField>
-            </p>
-            <p>Here, set the # of rooms in your home:
-                <TextField placeholder="# of Rooms" value={numRooms} onChange={(e) => setNumRooms(e.target.value)}></TextField>
-            </p>
-            <p>Here, set the # of bedrooms in your home:
-                <TextField placeholder="# of Bedrooms" value={numBedrooms} onChange={(e) => setNumBedrooms(e.target.value)}></TextField>
-            </p>
-            <p>Here, set the # of people in your home:
-                <TextField placeholder="# of People" value={numPeople} onChange={(e) => setNumPeople(e.target.value)}></TextField>
-            </p>
-            <p>Here, set the total heat/cooled square footage:
-                <TextField placeholder="# of People" value={numHeatCoolFootage} onChange={(e) => setNumHeatCoolFootage(e.target.value)}></TextField>
-            </p>
-            <p>Here, set the total heating/cooling degree days:
-                <TextField placeholder="# of People" value={numHeatCoolDays} onChange={(e) => setNumheatCoolDays(e.target.value)}></TextField>
-            </p>
-            <Button variant='contained' onClick={testPythonCall}>Send Python Data</Button>
-            <p>Here, set the your latitude:
-                <TextField placeholder="# of People" value={latitude} onChange={(e) => setLatitude(e.target.value)}></TextField>
-            </p>
-            <p>Here, set the your longitude:
-                <TextField placeholder="# of People" value={longitude} onChange={(e) => setLongitude(e.target.value)}></TextField>
-            </p>
-            <div>
-                <Button variant='contained' onClick={getSolarData}>Get Solar Data (input lattitude and longitude first)</Button>
-            </div>
-        </div>
-    )
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: "flex",
+          minHeight: "100vh",
+          backgroundColor: "#000",
+          color: "#fff",
+          fontFamily: "Quicksand, sans-serif",
+        }}
+      >
+        {/* Sidebar */}
+        <Box
+          sx={{
+            width: 240,
+            backgroundColor: "#111",
+            padding: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: 2,
+            borderRight: "1px solid #222",
+          }}
+        >
+          <Button 
+            onClick={logoutUser}
+            variant="contained"
+            sx={{
+            textTransform: "none",
+            background: "linear-gradient(90deg, #3DC787 0%, #55C923 100%)",
+            boxShadow: "0 4px 20px rgba(85, 201, 35, 0.3)",
+            "&:hover": {
+                background:
+                "linear-gradient(90deg, #55C923 0%, #3DC787 100%)",
+            },
+            }}
+          >
+            LOGOUT
+          </Button>
+          <img src="EcoTrack.svg" alt="EcoTrack Logo" width="140px" />
+          <Typography
+            sx={{
+                fontFamily: "Quicksand, sans-serif",
+                fontSize: 18,
+                color: "#ccc",
+                lineHeight: 1.6,
+            }}
+          >
+            {localStorage.getItem("username")}
+          </Typography>
+          <p></p>
+          <Divider sx={{ width: "100%", borderColor: "#333", my: 2 }} />
+          <List sx={{ width: "100%" }}>
+            <ListItem button>
+              <ListItemIcon sx={{ color: "#55C923" }}>
+                <BarChartIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Live Monitoring"
+                primaryTypographyProps={{ fontWeight: 600 }}
+              />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon sx={{ color: "#55C923" }}>
+                <InsightsIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Reports"
+                primaryTypographyProps={{ fontWeight: 600 }}
+              />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon sx={{ color: "#55C923" }}>
+                <MonetizationOnIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Cost Estimates"
+                primaryTypographyProps={{ fontWeight: 600 }}
+              />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon sx={{ color: "#55C923" }}>
+                <BoltIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Usage Tips"
+                primaryTypographyProps={{ fontWeight: 600 }}
+              />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon sx={{ color: "#55C923" }}>
+                <ScheduleIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Schedule"
+                primaryTypographyProps={{ fontWeight: 600 }}
+              />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon sx={{ color: "#55C923" }}>
+                <SpaIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Green Savings"
+                primaryTypographyProps={{ fontWeight: 600 }}
+              />
+            </ListItem>
+          </List>
+        </Box>
+
+        {/* Main Content */}
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            p: 4,
+            gap:"8px"
+          }}
+        >
+            <Box sx={{ height: 500, width: 800 }}>
+            <LiveEnergyChart />
+            </Box>
+            <Button 
+                onClick={testPythonCall}
+                variant="contained"
+                sx={{
+                textTransform: "none",
+                background: "linear-gradient(90deg, #3DC787 0%, #55C923 100%)",
+                boxShadow: "0 4px 20px rgba(85, 201, 35, 0.3)",
+                "&:hover": {
+                    background:
+                    "linear-gradient(90deg, #55C923 0%, #3DC787 100%)",
+                },
+                }}
+            >
+                Send Python Data
+            </Button>
+            
+            <Box sx={{display: "flex", alignItems: "center", gap: "8px"}}>
+                <Typography
+                    sx={{
+                        fontFamily: "Quicksand, sans-serif",
+                        fontSize: 18,
+                        color: "#ccc",
+                        lineHeight: 1.6,
+                    }}
+                >
+                    Here, set your latitude:
+                </Typography>
+                <TextField sx={{backgroundColor:"white"}} size="small" placeholder="Latitude Coords" value={latitude} onChange={(e) => setLatitude(e.target.value)}></TextField>
+            </Box>
+
+            <Box sx={{display: "flex", alignItems: "center", gap: "8px"}}>
+                <Typography
+                    sx={{
+                        fontFamily: "Quicksand, sans-serif",
+                        fontSize: 18,
+                        color: "#ccc",
+                        lineHeight: 1.6,
+                    }}
+                >
+                    Here, set your longitude:
+                </Typography>
+                <TextField sx={{backgroundColor:"white"}} size="small" placeholder="Longitude Coords" value={longitude} onChange={(e) => setLongitude(e.target.value)}></TextField>
+            </Box>
+
+            <Button 
+                onClick={getSolarData}
+                variant="contained"
+                sx={{
+                textTransform: "none",
+                background: "linear-gradient(90deg, #3DC787 0%, #55C923 100%)",
+                boxShadow: "0 4px 20px rgba(85, 201, 35, 0.3)",
+                "&:hover": {
+                    background:
+                    "linear-gradient(90deg, #55C923 0%, #3DC787 100%)",
+                },
+                }}
+            >
+                Get Solar Data (input lattitude and longitude first)
+            </Button>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+};
