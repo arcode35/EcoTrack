a322c40aa2ea546cf4857f2fa303cc72873f24fc
MahdMalik <mahdmahdmalik@gmail.com>
2025-04-02 21:46:10 -0500

    added call to get energy rates

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ app.use(cors()); // Enable CORS for cross-origin requests. Want to protect again
 //gets the routes listed in the file given from the path ./routes/users
 const userRoutes = require("./routes/users");
 const solarRoutes = require("./routes/solar");
+const utilRateRoutes = require("./routes/utilRates");
 
 //parses json automatically
 app.use(bodyParser.json());
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 //adds the userRoutes and solarRoutes to the app such that they can be called.
 app.use("/", userRoutes);
 app.use("/", solarRoutes)
+app.use("/", utilRateRoutes)
 
 
 // Sample route
