64353a76cf46a4510d1d098244a9cd486154d5a7
MahdMalik <mahdmahdmalik@gmail.com>
2025-04-05 02:28:10 -0500

    implemented solar data for when panel count is unknown.

--- backend/routes/solar.js
+++ backend/routes/solar.js
@@ -8,6 +8,58 @@ require('dotenv').config();
 //has it parse json automatically.
 router.use(bodyParser.json());
 
+const getFinanceIndex = (financeArr, monthlyCost) => {
+    let lowerBound = 0
+    //we'll say the upper bound is inclusive
+    let upperBound = financeArr.length - 1
+    let i = -1
+    let financialAnalysisIndex = 0
+    //going to do a binary search to find the solar panel config closest in monthly cost to what was passed in
+    while(true)
+    {
+        //get midpoint between bounds first
+        i = Math.floor((upperBound + lowerBound) / 2)
+        //if we found our target index, set it and break
+        if(financeArr[i].monthlyBill.units == monthlyCost)
+        {
+            financialAnalysisIndex = i
+            break
+        }
+        //if midpoint is more than our monthly cost, limit our upper bound to that and retry
+        else if(financeArr[i].monthlyBill.units > monthlyCost)
+        {
+            upperBound = i - 1
+        }
+        //if midpoint is less than our monthly cost, limit our lower bound to that and retry
+        else if(financeArr[i].monthlyBill.units < monthlyCost)
+        {
+            lowerBound = i + 1
+        }
+        //end condition to while loop. Now we need to decide which would be better to take as the index, and take that one
+        if(upperBound < lowerBound)
+        {
+            //check first if one of the bounds is out of bounds; makes it clear we should take the other oen in that case
+            if(upperBound < 0)
+            {
+                financialAnalysisIndex = lowerBound
+                break; 
+            }
+            if(lowerBound > financeArr.length - 1)
+            {
+                financialAnalysisIndex = upperBound
+                break;
+            }
+            //otherwise, compare disstances to the monthly cost for each bound, take the one closer to the true monthly cost
+            let lowerDist = Math.abs(financeArr[lowerBound].monthlyBill.units - monthlyCost)
+            let upperDist = Math.abs(financeArr[upperBound].monthlyBill.units - monthlyCost)
+            financialAnalysisIndex = (lowerDist < upperDist) ? lowerBound : upperBound
+            break
+        }
+    }
+    console.log("final index: " + financialAnalysisIndex)
+    return financialAnalysisIndex
+}
+
 //gets solar data
 router.post("/solar/getData", async (req, res) => {
     const { latitude, longitude, monthlyCost } = req.body;
@@ -19,70 +71,24 @@ router.post("/solar/getData", async (req, res) => {
         //wait for data, then return it
         const dataJson = solarData.data
         const financialAnalysisArray = dataJson.solarPotential.financialAnalyses
-        let lowerBound = 0
-        //we'll say the upper bound is inclusive
-        let upperBound = financialAnalysisArray.length - 1
-        let i = -1
-        let financialAnalysisIndex = 0
-        //going to do a binary search to find the solar panel config closest in monthly cost to what was passed in
-        while(true)
-        {
-            i = Math.floor((upperBound + lowerBound) / 2)
-            if(i < 0 || i > financialAnalysisArray.length - 1)
-            {
-                console.log("AAAA")
-                break
-            }
-            console.log(financialAnalysisArray[i].monthlyBill.units)
-            console.log("i: " + i)
-            console.log("upper bound: " + upperBound)
-            console.log("lower bound: " + lowerBound)
-            if(financialAnalysisArray[i].monthlyBill.units == monthlyCost)
-            {
-                financialAnalysisIndex = i
-                break
-            }
-            else if(financialAnalysisArray[i].monthlyBill.units > monthlyCost)
-            {
-                console.log("go down")
-                upperBound = i - 1
-            }
-            else if(financialAnalysisArray[i].monthlyBill.units < monthlyCost)
-            {
-                console.log("go up")
-                lowerBound = i + 1
-            }
-            if(upperBound < lowerBound)
-            {
-                if(upperBound < 0)
-                {
-                    financialAnalysisIndex = lowerBound
-                    break; 
-                }
-                if(lowerBound > financialAnalysisArray.length - 1)
-                {
-                    financialAnalysisIndex = upperBound
-                    break;
-                }
-                let lowerDist = Math.abs(financialAnalysisArray[lowerBound].monthlyBill.units - monthlyCost)
-                let upperDist = Math.abs(financialAnalysisArray[upperBound].monthlyBill.units - monthlyCost)
-                financialAnalysisIndex = (lowerDist < upperDist) ? lowerBound : upperBound
-                break
-            }
-        }
-        console.log("final index: " + financialAnalysisIndex)
+        const financialAnalysisIndex = getFinanceIndex(financialAnalysisArray, monthlyCost)
+        //get according to the object structure
         const panelConfigIndex = financialAnalysisArray[financialAnalysisIndex].panelConfigIndex
+        //if we got -1, that means that we actually shouldn't use solar panels.
         if(panelConfigIndex == -1)
         {
-            return res.json({success: false, error: "No solar panels would help actually!"})
+            return res.json({success: false, error: "We actually don't need solar panels!"})
         }
-        console.log("config index: " + panelConfigIndex)
         const numPanels = dataJson.solarPotential.solarPanelConfigs[panelConfigIndex].panelsCount
-        return res.json({success: true, data: dataJson, numPanels: numPanels})
+        //according to the structure
+        const costBesidesSolarPanels = Number(financialAnalysisArray[financialAnalysisIndex].financialDetails.remainingLifetimeUtilityBill.units)
+        const solarPanelInstallationCost = Number(financialAnalysisArray[financialAnalysisIndex].cashPurchaseSavings.upfrontCost.units)
+        //adding the cost besides solar panels with the solar panel installation cost. Note that this is over 20 years btw.
+        return res.json({success: true, data: dataJson, numPanels: numPanels, totalSolarCost: costBesidesSolarPanels + solarPanelInstallationCost})
     }
     catch (error)
     {
-        res.json({success: false, error: error})
+        return res.json({success: false, error: error})
     }
 })
 
